f0af25b718aac3ef7e7bc88ae932d74f0f64a9d2
vedhpatel18 <halpanivedh189@gmail.com>
2026-03-14 18:44:07 +0530

    Level1 added

---
+++ MainBranch.java
@@ -0,0 +1,15 @@
+/* 
+@version 2.0
+@autor Vedh
+ */
+ 
+class MainBranch{
+	public static void main(String[] args ){
+		
+		String msg = "This is Main Branch";
+		
+		System.out.println(msg);
+		
+	}
+}
+ 
